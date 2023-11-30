f03922266a3e9085a9f7b5a5d55f702ea0a2e6a1
Mizu <psdk55052004@gmail.com>
2023-11-30 15:20:49 +0800

    匯入 highlight 套件

--- src/main.js
+++ src/main.js
@@ -7,10 +7,20 @@ import 'bootstrap'
 
 import App from './App.vue'
 
+import hljs from 'highlight.js/lib/core'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
+import javascript from 'highlight.js/lib/languages/javascript'
+import json from 'highlight.js/lib/languages/json'
+import 'highlight.js/styles/dark.css'
+
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('json', json)
+
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
+app.use(hljsVuePlugin)
 
 app.mount('#app')
