cbc0924e730e0f1b20a1928414348a6fd8f7b65a
Mizu <psdk55052004@gmail.com>
2023-12-01 14:30:55 +0800

    新增 拖曳的狀態

--- src/components/Demo1.vue
+++ src/components/Demo1.vue
@@ -20,10 +20,11 @@
         <!-- 拖曳元件 -->
         <Draggable ref="draggableRef"
                    class="list-group"
-                   item-key="id"
                    :list="data"
+                   item-key="id"
                    :disabled="!enabled"
                    chosen-class="active"
+                   ghost-class="bg-danger"
                    animation="500"
                    @start="handleStart"
                    @end="handleEnd"
@@ -34,7 +35,7 @@
                    @move="handleMove">
           <template #item="{element}">
             <div class="list-group-item user-select-none"
-                 :class="enabled?'cursor-move':'cursor-default'"
+                 :class="[enabled?'cursor-move':'cursor-default',element.className]"
                  style="padding: 10px 30px;">
               {{ element.id+1 }}. {{ element.name }}
             </div>
@@ -46,8 +47,59 @@
         <!-- 程式程式碼區塊 -->
         <highlightjs language="json"
                      :code="JSON.stringify(data, null, 2)" />
-
       </div>
+    </div>
+
+    <div style="margin-left:100px;">
+      <!-- <h3>事件觀察區</h3> -->
+      <ul>
+        <li class="text-20">
+          拖曳中：
+          <span :class="dragging?'text-success':'text-gray'">
+             {{ dragging?'O':'X' }}
+          </span>
+        </li>
+        <li class="text-20">
+          目前拖曳的項目：
+          <span :class="moveTarget.name||'text-gray'">
+            {{ moveTarget.name||'( 無 )' }}
+          </span>
+        </li>
+        <li class="text-20">
+          被拖曳的項目：
+          <span :class="currentDraggedItem.name||'text-gray'">
+            {{ currentDraggedItem.name||'( 無 )' }}
+          </span>
+        </li>
+        <li class="text-20">
+          拖曳後的目標項目：
+          <span :class="targetItemAfterDrag||'text-gray'">
+            {{ targetItemAfterDrag||'( 無 )' }}
+          </span>
+        </li>
+        <li class="text-20">
+          位置改變(move事件)：
+          <span>
+            <template v-if="isMoveDemoShow">
+              {{ currentDraggedItem.name }} 從第 {{ moveResult.oldIndex+1 }} 個移動到第 {{ moveResult.newIndex+1 }} 個
+            </template>
+            <template v-else>
+              <span class="text-gray">( 無 )</span>
+            </template>
+          </span>
+        </li>
+        <li class="text-20">
+          位置改變(update事件)：
+          <span>
+            <template v-if="isUpdateDemoShow">
+              {{ currentDraggedItem.name }} 從第 {{ updateResult.oldIndex+1 }} 個移動到第 {{ updateResult.newIndex+1 }} 個
+            </template>
+            <template v-else>
+              <span class="text-gray">( 無 )</span>
+            </template>
+          </span>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -87,36 +139,66 @@ const data = reactive([
     id: 6
   }
 ])
+const tempDate = ref([]) // 用來判斷當前交換、被交換的對象是誰
 const enabled = ref(true) // 是否啟用
 const dragging = ref(false) // 是否正在拖曳
-// const draggableRef = ref(null)
+const moveTarget = ref('') // 被拖曳的項目
+const currentDraggedItem = ref('') // 目前拖曳的項目
+const targetItemAfterDrag = ref('') // 拖曳後的目標項目
+const isUpdateDemoShow = ref(false)
+const isMoveDemoShow = ref(false)
+const updateResult = ref({}) // update 事件結果
+const moveResult = ref({
+  oldIndex: '',
+  newIndex: ''
+}) // move 事件結果
 
 onMounted(() => {
   // console.log(draggableRef.value)
+
+  tempDate.value = JSON.parse(JSON.stringify(data))
 })
 
 function handleStart (e) {
-  console.log('start事件(開始拖曳)', e)
+  // console.log('start事件(開始拖曳)', e)
   dragging.value = true
 }
 function handleEnd (e) {
-  console.log('end事件(拖曳結束)', e)
+  // console.log('end事件(拖曳結束)', e)
   dragging.value = false
+  moveTarget.value = ''
 }
 function handleUpdate (e) {
-  console.log('update事件(在拖曳結束後觸發，提供了整個列表的新順序)', e)
+  // console.log('update事件(在拖曳結束後觸發，提供了整個列表的新順序)', e)
+
+  updateResult.value = {
+    newIndex: e.newIndex,
+    oldIndex: e.oldIndex
+  }
+
+  // 每次交換成功都要更新tempDate, 才能用最新的tempDate來判斷當前對象、被交換的對象
+  tempDate.value = JSON.parse(JSON.stringify(data))
 }
 function handleChoose (e) {
-  console.log('choose事件 (選擇了某個項目)', e)
+  // console.log('choose事件 (選擇了某個項目)', e)
+  moveTarget.value = data[e.oldDraggableIndex]
+  currentDraggedItem.value = data[e.oldDraggableIndex]
 }
 function handleUnchoose (e) {
-  console.log('handleUnchoose事件 (取消選擇了某個項目)', e)
+  // console.log('handleUnchoose事件 (取消選擇了某個項目)', e)
 }
 function handleSort (e) {
-  console.log('handleSort事件 (排序改變了)', e)
+  // console.log('handleSort事件 (排序改變了)', e)
+  isUpdateDemoShow.value = true
 }
 function handleMove (e) {
+  isMoveDemoShow.value = true
   console.log('handleMove事件 (每移動一次排序就會觸發[回傳拖曳對象])', e)
+  const preIndex = e.related.__draggable_context.index
+  targetItemAfterDrag.value = tempDate.value[preIndex].name
+
+  moveResult.value.oldIndex = e.dragged.__draggable_context.index
+  moveResult.value.newIndex = e.related.__draggable_context.index
 }
 
 </script>
