cdddcf116b9f82c21a61e023b6304860b40dabbd
Mizu <psdk55052004@gmail.com>
2023-12-04 18:01:15 +0800

    修改 專案名稱

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/draggable/', //! 請輸入儲存庫名稱
+  base: '/vueDraggable/', //! 請輸入儲存庫名稱
   plugins: [vue()],
   resolve: {
     alias: [
